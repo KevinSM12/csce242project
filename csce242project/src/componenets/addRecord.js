0c48ca78587ddda9cc38502c4af14865b307c5ad
Kevin SM <coolkevino@gmail.com>
2024-11-17 21:44:21 -0500

    u

--- csce242project/src/componenets/addRecord.js
+++ csce242project/src/componenets/addRecord.js
@@ -24,7 +24,7 @@ const AddRecord = (props) => {
     const formData = new FormData(event.target);
     console.log(...formData);
 
-    const response = await fetch("https://csce242backend.onrender.com",{
+    const response = await fetch("https://csce242backend.onrender.com/api/records",{
       method:"POST",
       body:formData
     });
